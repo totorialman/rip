66abc271970b3736cd1ee40af66b9bed5f667b6b
totorialman <workbook2003@gmail.com>
2024-10-10 13:13:59 +0300

    lab3

--- bmstu_lab/test.py
+++
@@ -1,9 +0,0 @@
-t = int(input())  # Читаем количество наборов данных
-for i in range(t):
-    s1 = input().strip()  # Читаем вывод участника
-    s2 = input().strip()  # Читаем эталонный ответ жюри
-    if s1 == s2:
-        print("HAPPY_NEW_YEAR!")  # Верный ответ
-    else:
-        print("WA")  # Неправильный ответ
-    input()
--- rent_server/Untitled-1.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(){
-    std::cout<<"trest";
-    return 0;
-}
