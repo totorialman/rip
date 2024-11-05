469c28d71f9f71d234123af601554d8fce7ac1fb
totorialman <workbook2003@gmail.com>
2024-11-05 15:09:30 +0300

    lab3

--- lab3/lab3/settings.py
+++ lab3/lab3/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'USER': 'admin',
         'PASSWORD': 'admin',
         #'HOST': 'localhost',
-        'HOST': '5.42.99.7',
+        'HOST': '91.184.243.246',
         'PORT': 7474, # Стандартный порт PostgreSQL
         
     }
@@ -140,5 +140,5 @@ AWS_STORAGE_BUCKET_NAME = 'logo'
 AWS_ACCESS_KEY_ID = 'minio'
 AWS_SECRET_ACCESS_KEY = 'minio124'
 #AWS_S3_ENDPOINT_URL = 'localhost:9000'
-AWS_S3_ENDPOINT_URL = '5.42.99.7:9000'
+AWS_S3_ENDPOINT_URL = '91.184.243.246:9000'
 MINIO_USE_SSL = False
