fce83f52805ac54073b653299410da4a421eb710
totorialman <workbook2003@gmail.com>
2024-11-11 14:07:52 +0300

    lab3

--- lab3/lab3/1.cpp
+++ lab3/lab3/1.cpp
@@ -1,58 +1,103 @@
 #include <iostream>
-#include <functional>
-#include <cstdlib>
-#include <ctime>
-#include <utility>
 
-// Функция для разбиения массива методом прохода двумя итераторами
-int partition(int arr[], int left, int right, std::function<bool(int, int)> comp) {
-    int randomIndex = left + std::rand() % (right - left + 1);
-    std::swap(arr[randomIndex], arr[right]);
-    int pivot = arr[right];
-    int i = right;
-    int j = right - 1;
+class Deque {
+ private:
+    int* data;
+    int head;
+    int tail;
+    int cap;
+    int size;
 
-    while (j >= left) {
-        if (comp(arr[j], pivot)) {
-            --j;
-        } else {
-            --i;
-            std::swap(arr[i], arr[j]);
-            --j;
+    void resize() {
+        int new_cap = cap * 2;
+        int* new_data = new int[new_cap];
+
+        for (int i = 0; i < size; ++i) {
+            new_data[i] = data[(head + i) % cap];
         }
-    }
-    std::swap(arr[i], arr[right]);
-    return i;
-}
 
-// Функция для поиска k-го порядкового элемента
-int quickSelect(int arr[], int left, int right, int k, std::function<bool(int, int)> comp) {
-    while (true) {
-        int pivot = partition(arr, left, right, comp);
-        if (pivot == k) {
-            return arr[pivot];
-        } else if (pivot < k) {
-            left = pivot + 1;
-        } else {
-            right = pivot - 1;
-        }
+        delete[] data;
+        data = new_data;
+        cap = new_cap;
+        head = 0;
+        tail = size;
     }
-}
 
-bool comp(int a, int b) {
-    return a < b;
-}
+ public:
+    explicit Deque(int start_cap = 4) : head(0), tail(0), size(0), cap(start_cap) {
+        data = new int[cap];
+    }
+
+    ~Deque() {
+        delete[] data;
+    }
+
+    void push_front(int value) {
+        if (size == cap) resize();
+        head = (head - 1 + cap) % cap;
+        data[head] = value;
+        size++;
+    }
+
+    void push_back(int value) {
+        if (size == cap) resize();
+        data[tail] = value;
+        tail = (tail + 1) % cap;
+        size++;
+    }
+
+    int pop_front() {
+        if (size == 0) return -1;
+        int value = data[head];
+        head = (head + 1) % cap;
+        size--;
+        return value;
+    }
+
+    int pop_back() {
+        if (size == 0) return -1;
+        tail = (tail - 1 + cap) % cap;
+        int value = data[tail];
+        size--;
+        return value;
+    }
+};
 
 int main() {
-    std::srand(std::time(0));
     int n;
     std::cin >> n;
-    int arr[n];
+
+    Deque deque;
+    bool is_correct = true;
+
     for (int i = 0; i < n; ++i) {
-        std::cin >> arr[i];
+        int command, expected_value;
+        std::cin >> command >> expected_value;
+
+        int result;
+        switch (command) {
+            case 1:  
+                deque.push_front(expected_value);
+                break;
+            case 2:  
+                result = deque.pop_front();
+                if (result != expected_value) is_correct = false;
+                break;
+            case 3:  
+                deque.push_back(expected_value);
+                break;
+            case 4:  
+                result = deque.pop_back();
+                if (result != expected_value) is_correct = false;
+                break;
+        }
+
+        if (!is_correct) {
+            std::cout << "NO" << std::endl;
+            return 0;
+        }
     }
-    std::cout << quickSelect(arr, 0, n - 1, n / 10, comp) << std::endl;
-    std::cout << quickSelect(arr, 0, n - 1, n / 2, comp) << std::endl;
-    std::cout << quickSelect(arr, 0, n - 1, 9 * n / 10, comp) << std::endl;
+
+    std::cout << "YES" << std::endl;
     return 0;
 }
